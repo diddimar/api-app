ad3c8b72d95bbc980f17b34abd79d9404664334e
David Mar Gunnarsson <dmrgunnarsson@gmail.com>
2016-11-15 04:45:08 +0000

    basic concert list added

--- app/concert/concert.component.ts
+++ app/concert/concert.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
     moduleId: module.id,
@@ -6,7 +7,22 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './concert.component.html'
 })
 export class ConcertComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit() { }
+    concerts;
+
+    constructor(
+         private http: Http
+    ) { }
+
+    ngOnInit() { 
+        this.getConcerts();
+    }
+
+    getConcerts() {
+    this.http.get('http://apis.is/concerts')
+    .map(res => res.json())
+    .subscribe((concert) => {
+      this.concerts = concert.results;
+    })
+  }
 }
